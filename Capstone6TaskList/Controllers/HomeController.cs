5494e8aeedeac42db8fa06e6ae837fff480b8f4c
unknown <adamhorn8@gmail.com>
2018-06-03 22:31:26 -0700

    first commit

---
+++ Capstone6TaskList/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Capstone6TaskList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone6TaskList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SearchTask()
+        {
+            return View();
+        }
+
+        public ActionResult SearchResult(int ID)
+        {
+            TaskListEntities ORM = new TaskListEntities();
+
+            ViewBag.Task = ORM.Tasks.Where(x => x.UserID == ID).ToList();
+
+            return View();
+        }
+
+    }
+}
